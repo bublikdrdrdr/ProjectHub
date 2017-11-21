d1d35d6540ce0433066e4a2e664281e8e0aa8589
Vadym Borys <bublik.drdrdr@gmail.com>
2017-11-21 16:48:48 +0100

    finished User model[2]

--- api/src/main/java/app/entities/db/User.java
+++ api/src/main/java/app/entities/db/User.java
@@ -1,7 +1,12 @@
 package app.entities.db;
 
 
+import app.Properties;
+import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.context.annotation.Lazy;
+
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 /**
  * Created by Bublik on 10-Nov-17.
@@ -11,12 +16,37 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @Column(name = "id")
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email")
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(length = Properties.db.nameLength, nullable = false)
+    private String name;
+
+    @Column(length = Properties.db.surnameLength, nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
+    @Column(name = "password_salt", nullable = false)
+    @JsonIgnore
+    private String passwordSalt;
+
+    @Column
+    @Lazy
+    @JsonIgnore
+    private byte[] image;
+
+    @Column
+    private Timestamp registered;
+
+    @Column(name = "last_login")
+    private Timestamp lastOnline;
 
     public User() {
     }
@@ -40,5 +70,61 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Timestamp getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Timestamp registered) {
+        this.registered = registered;
+    }
+
+    public Timestamp getLastOnline() {
+        return lastOnline;
+    }
+
+    public void setLastOnline(Timestamp lastOnline) {
+        this.lastOnline = lastOnline;
     }
 }
