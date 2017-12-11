a261b04d189d82900558a0cbd31f8c76a5392e92
Vadym Borys <bublik.drdrdr@gmail.com>
2017-12-12 00:38:41 +0100

    fixed user repository

--- api/src/main/java/app/entities/dao/UsersRepository.java
+++ api/src/main/java/app/entities/dao/UsersRepository.java
@@ -2,6 +2,7 @@ package app.entities.dao;
 
 import app.db.SessionHolder;
 import app.entities.db.User;
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +20,31 @@ public class UsersRepository {
     @Autowired
     private SessionHolder sessionHolder;
 
+    public static final int INCLUDE_IMAGE_FLAG = 0x00000001;
+    public static final int INCLUDE_PROJECTS_FLAG = 0x00000002;
+    public static final int INCLUDE_LIKES_FLAG = 0x00000004;
+
     public User getUser(long id){
+        return getUser(id, 0);
+    }
+
+    public User getUser(long id, int flags){
         try {
             Session session = sessionHolder.getSession();
             User user = session.get(User.class, id);
+            if (checkFlag(flags, INCLUDE_IMAGE_FLAG)) Hibernate.initialize(user.getImage());
+            if (checkFlag(flags, INCLUDE_PROJECTS_FLAG)) Hibernate.initialize(user.getProjects());
+            if (checkFlag(flags, INCLUDE_LIKES_FLAG)) Hibernate.initialize(user.getLikedProjects());
             session.close();
             return user;
         } catch (HibernateException e){
             sessionHolder.closeSession();
             throw e;
         }
+    }
+
+    private boolean checkFlag(int value, int flag){
+        return ((value&flag)==flag);
     }
 
     public User getUser(String email){
