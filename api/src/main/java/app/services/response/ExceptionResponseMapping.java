2a1da5a8a6e82f6e953692a658c30e056175bf35
Vadym Borys <bublik.drdrdr@gmail.com>
2017-12-22 17:42:55 +0200

    added response factory, exception/http status mapping

---
+++ api/src/main/java/app/services/response/ExceptionResponseMapping.java
@@ -0,0 +1,154 @@
+package app.services.response;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by Bublik on 18-Dec-17.
+ * todo: test
+ */
+public class ExceptionResponseMapping implements Cloneable{
+
+    private LinkedList<ExceptionStatusPair> pairs = new LinkedList<>();
+    private HttpStatus defaultStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    public ExceptionResponseMapping() {
+    }
+
+    public ExceptionResponseMapping(ExceptionResponseMapping source) {
+        this(source.pairs);
+    }
+
+    private ExceptionResponseMapping(List<ExceptionStatusPair> pairs){
+        //this.pairs.addAll(pairs.stream().map(ExceptionStatusPair::clone).collect(Collectors.toList()));
+        for (ExceptionStatusPair pair: pairs){
+            this.pairs.add(pair.clone());
+        }
+        // TODO: 22-Dec-17 compare speed
+    }
+
+    public void addAll(ExceptionResponseMapping mapping){
+        mapping.pairs.forEach(this::set);
+    }
+
+    private void set(ExceptionStatusPair pair){
+        set(pair.e, pair.s);
+    }
+
+    public void set(Exception e, HttpStatus status){
+        ListIterator<ExceptionStatusPair> iterator = pairs.listIterator();
+        while (iterator.hasNext()){
+            ExceptionStatusPair current = iterator.next();
+            if (equal(current.e, e)){
+                current.s = status;
+                return;
+            } else if (instance(e, current.e)) {
+                iterator.previous();
+                iterator.add(new ExceptionStatusPair(e, status));
+                return;
+            }
+        }
+        pairs.add(new ExceptionStatusPair(e, status));
+    }
+
+    public HttpStatus get(Exception e){
+        for (ExceptionStatusPair pair: pairs){
+            if (instance(e, pair.e)){
+                return pair.s;
+            }
+        }
+        return defaultStatus;
+    }
+
+    public void remove(Exception e){
+        remove(e, false, false);
+    }
+
+    public void removeInstance(Exception e){
+        remove(e, true, false);
+    }
+
+    public void removeAllInstances(Exception e){
+        remove(e, true, true);
+    }
+
+    private void remove(Exception e, boolean instance, boolean all){
+        Iterator<ExceptionStatusPair> iterator = pairs.iterator();
+        while (iterator.hasNext()){
+            ExceptionStatusPair pair = iterator.next();
+            if (!instance && equal(e, pair.e)){
+                iterator.remove();
+                return;
+            } else if (instance && instance(e, pair.e)){
+                iterator.remove();
+                if (!all) return;
+            }
+        }
+    }
+
+    private boolean instance(Exception request, Exception registered){
+        return registered.getClass().isInstance(request);
+    }
+
+    private boolean equal(Exception e1, Exception e2){
+        return e1.getClass().equals(e2.getClass());
+    }
+
+    public void clear(){
+        pairs.clear();
+    }
+
+
+    private class ExceptionStatusPair implements Cloneable{
+        Exception e;
+        HttpStatus s;
+
+        public ExceptionStatusPair() {
+        }
+
+        ExceptionStatusPair(Exception e, HttpStatus s) {
+            this.e = e;
+            this.s = s;
+        }
+
+        @Override
+        protected ExceptionStatusPair clone() {
+            return new ExceptionStatusPair(e,s);
+        }
+
+        @Override
+        public String toString() {
+            return e.getClass().getSimpleName()+":"+s.name()+"("+s.toString()+")";
+        }
+    }
+
+    public HttpStatus getDefaultStatus() {
+        return defaultStatus;
+    }
+
+    public void setDefaultStatus(HttpStatus defaultStatus) {
+        this.defaultStatus = defaultStatus;
+    }
+
+    @Override
+    protected ExceptionResponseMapping clone() {
+        ExceptionResponseMapping erm = new ExceptionResponseMapping(this);
+        return erm;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        boolean first = true;
+        for (ExceptionStatusPair pair: pairs){
+            if (first) first = false; else b.append(", ");
+            b.append(pair.toString());
+        }
+        return b.append(']').toString();
+    }
+}
